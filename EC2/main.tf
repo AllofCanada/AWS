9c658647f33b5626fb993e9f271641a0d5d15689
mafarhan72 <mafarhan1234@gmail.com>
2026-04-20 13:31:54 -0400

    adding email trigger

--- EC2/main.tf
+++ EC2/main.tf
@@ -12,3 +12,11 @@ provider "aws" {
   region= "ca-central-1"
 }
 
+resource "aws_instance" "example" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
