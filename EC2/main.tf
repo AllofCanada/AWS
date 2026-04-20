2f680c24c67567bd0386fa26ffeb0decc1daafea
mafarhan72 <mafarhan1234@gmail.com>
2026-04-20 13:43:34 -0400

    removing the instance

--- EC2/main.tf
+++ EC2/main.tf
@@ -19,11 +19,11 @@ provider "aws" {
   region= "ca-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-  instance_type = "t2.micro"
+# resource "aws_instance" "example" {
+#   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
