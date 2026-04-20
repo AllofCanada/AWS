7892a4f118423b5d77967142b6ac859839871056
mafarhan72 <mafarhan1234@gmail.com>
2026-04-20 11:58:30 -0400

    first ec2 instance

---
+++ EC2/main.tf
@@ -0,0 +1,22 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region= "ca-central-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
